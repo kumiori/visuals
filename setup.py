ba81ec968a0d0ddabb1f2216cba49ca3d82ce656
kumiori <a.leon.baldelli@gmail.com>
2025-05-27 14:54:31 +0200

    merge

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='visuals',
-    version='0.1',
+    name="visuals",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        'matplotlib',
+        "matplotlib",
         # add other dependencies if needed
     ],
-    author='Andrés A León Baldelli',
-    author_email='leon.baldelli@cnrs.fr',
-    description='Default plotting settings and utilities.',
-    url='https://github.com/kumiori/visuals',
+    author="Andrés A León Baldelli",
+    author_email="leon.baldelli@cnrs.fr",
+    description="Default plotting settings and utilities.",
+    url="https://github.com/kumiori3/visuals",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
